0e2edf036b66739374e504a500ffad9542276206
Cory Dawson <cdawson@axon.com>
2026-02-03 13:50:32 +0700

    Update SHA256 for v1.0.0

--- bamboo-switch.rb
+++ bamboo-switch.rb
@@ -5,7 +5,7 @@ class BambooSwitch < Formula
   desc "Profile manager for Bambu Studio on macOS"
   homepage "https://github.com/c0ryd/bamboo-switch"
   url "https://github.com/c0ryd/bamboo-switch/releases/download/v1.0.0/bamboo-switch"
-  sha256 "PLACEHOLDER_SHA256"
+  sha256 "0c074c78796f27a4bc7eba5db2c6f4f8e666ce48359712b5843d71e42aa116ed"
   license "MIT"
 
   def install
